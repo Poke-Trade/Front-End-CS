92d55490d035e0186470fc7a02a7fcc5b31871ad
Abdi Farah <abdirahman.z.farah@gmail.com>
2020-04-27 14:22:24 -0700

    Login/Register

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,13 +2,22 @@ import React, { useState, useEffect } from "react";
 import { Switch, Route, Link } from "react-router-dom";
 
 //Components
-
+import Home from "./components/Home";
+import Register from "./components/RegisterForm";
+import Game from "./components/Game";
 // Styles
 
 function App() {
   return (
     <div className="App">
-      <h1>Pokeballs</h1>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/poketrade" component={Game} />
+        <Route path="/register" component={Register} />
+      </Switch>
+      {/* <Home /> */}
+
+      {/* <h1>Pokeballs</h1> */}
     </div>
   );
 }
--- my-app/src/components/LoginForm.jsx
+++ my-app/src/components/LoginForm.jsx
@@ -1,11 +1,72 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import ToolBar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+// import Link from "@material-ui/core/Link";
+import Container from "@material-ui/core/Container";
+
+const useStyles = makeStyles({
+  paper: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  form: {
+    width: "100%",
+  },
+  submit: {},
+});
 
 const Login = () => {
+  const classes = useStyles();
   return (
-    <div>
-      {" "}
-      <p>Login Form</p>{" "}
-    </div>
+    <Container maxWidth="xs">
+      <Typography variant="h5">Sign in</Typography>
+      <div className={classes.paper}>
+        <form className={classes.form}>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+          />
+          <Button
+            component={Link}
+            to="/poketrade"
+            fullWidth
+            variant="contained"
+            color="primary"
+            // className={classes.submit}
+          >
+            Sign In
+          </Button>
+          <Grid container>
+            <Grid item>
+              <Link to="/register" variant="body2">
+                {"Don't have an account? Sign Up"}
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+    </Container>
   );
 };
 
